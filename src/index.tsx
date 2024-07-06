43a5eef9fc481b684a5f85ad2e91eee1059b6656
shailjasarawagi <shailjasarawagi@gmail.com>
2024-07-06 19:45:45 +0545

    Added spellList,Spell Deatils and favouriteList
    -Initialized store for fetching data of spell and details of spell
    -Created SpellList and spell Details Page
    -Create Spell Favourite PAge with remove feature of spell
    -Used react-window for virtualization of such big list
    -Installed react-icons for icons of favourite
    -Created SpellRow element for eachelement of list

--- src/index.tsx
+++ src/index.tsx
@@ -4,23 +4,39 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-
+import { setupStore } from "./store/store";
 import {
   BrowserRouter as Router,
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import SpellList from "./components/templates/Spell/SpellList";
+import SpellDetailsPage from "./components/modules/Spell/SpellDetails";
+import FavoritesList from "./components/modules/Spell/SpellFavoriteList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/spells",
+        element: <SpellList />,
+      },
+      { path: "/spell/:index/", element: <SpellDetailsPage /> },
+      { path: "/favorites", element: <FavoritesList /> },
+    ],
   },
 ]);
 
+const store = setupStore();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
 
 reportWebVitals();
